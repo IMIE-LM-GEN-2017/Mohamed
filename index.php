9e06a86207915729ffdc3fa05bb12f79c239d086
Mennajy <mennajy@live.fr>
2017-08-25 16:54:02 +0200

    Refonte complete

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="css/semantic.css">
+      <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="css/semantic.min.js"></script>
+    <title>Portfolio</title>
+    </head>
+  <body>
+
+    <!-- Menu -->
+
+    <div class="ui secondary pointing huge menu">
+      <a class= "item" href="#">Home </a>
+      <a class="item">Messages </a>
+      <a class="item">Amis </a>
+    </div>
+
+    <!-- Slider -->
+
+    <!-- Acceuil -->
+
+    <div class="ui internally celled grid">
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <h1 class="ui center aligned icon header"><i class="circular huge users icon"></i>
+            <p> Amis </p>
+          </h1>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+
+      <!-- Connaissances -->
+
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <div class="ui raised fluid card">
+            <div class="content">
+              <div class="header">Mes connaissances</div>
+              <div class="description">
+                <p></p>
+              </div>
+            </div>
+            <div class="extra content">
+              <div class="left floated"><img class="ui left tiny rounded image" src="images/php.png"> Bar </div>
+            </div>
+            <div class="extra content">
+              <div class="left floated"><img class="ui left tiny rounded image" src="images/html_css.png"> Bar</div>
+            </div>
+          </div>
+          <p></p>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+      <!-- Connaissances -->
+
+      <!-- Framework -->
+
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <div class="ui three column grid">
+          <div class="column">
+          <div class="ui fluid card">
+            <div class="image">
+              <img src="images/bootstrap.png">
+            </div>
+            <div class="content">
+              <a class="header">Bootstrap</a>
+            </div>
+          </div>
+          </div>
+          <div class="column">
+          <div class="ui fluid card">
+            <div class="image">
+              <img src="images/materialize.png">
+            </div>
+            <div class="content">
+              <a class="header">Materialize</a>
+            </div>
+          </div>
+          </div>
+          <div class="column">
+          <div class="ui fluid card">
+            <div class="image">
+              <img src="images/semanticui.png">
+            </div>
+            <div class="content">
+              <a class="header">Semantic Ui</a>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+      <!-- Framework -->
+
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <p></p>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <p></p>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="three wide column">
+          <img>
+        </div>
+        <div class="ten wide column">
+          <p></p>
+        </div>
+        <div class="three wide column">
+          <img>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+    <!-- Framework Utilisé -->
+    <div class="ui internally celled grid">
+
+    </div>
+
+    <!-- Liens vers les reseaux sociaux -->
+
+    <!-- Footer -->
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- grille -->
+<div class="ui internally celled grid">
+  <div class="row">
+    <div class="three wide column">
+      <img>
+    </div>
+    <div class="ten wide column">
+      <p></p>
+    </div>
+    <div class="three wide column">
+      <img>
+    </div>
+  </div>
+</div>
